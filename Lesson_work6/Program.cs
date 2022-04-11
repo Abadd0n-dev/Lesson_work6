74fca730e96c0a288728d2d22e61dbae8f1e9b2e
rkutaraev@list.ru <rkutaraev@list.ru>
2022-04-11 20:05:38 +0300

    Консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс.

--- Lesson_work6/Program.cs
+++ Lesson_work6/Program.cs
@@ -11,7 +11,33 @@ namespace Lesson_work6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\tTask Manager");
+            while (true)
+            {
+                Console.Clear();
+                Process[] processes = Process.GetProcesses();
 
+                for (int i = 0; i < processes.Length; i++)
+                {
+
+                    Console.WriteLine($"ID: \t{processes[i].Id} \tName: \t{processes[i].ProcessName}");
+
+                }
+
+                Console.Write("Enter the name of the process you want to remove: ");
+                string name = Console.ReadLine();
+
+                try
+                {
+                    processes.First(p => p.ProcessName.ToLower() == name.ToLower()).Kill();
+                    Console.WriteLine($"{name} deleted!");
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine($"Process {name} not found!");
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
